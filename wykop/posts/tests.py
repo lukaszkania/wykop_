e78db44491fe934bd1a7fc87ad961ca972a3ddd1
Lukasz Kania <lukasz.kania199@gmail.com>
2019-05-14 20:31:27 +0200

    Add

--- wykop/posts/tests.py
+++ wykop/posts/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class ReactionsTest(TestCase):
+    def test_true(self):
+        self.assertTrue(True)
+        self.assertFalse(False)
